861e1ea5aefe9ad62ecd603913963bf22f40fb44
jatin <jatinranjan8@gmail.com>
2023-08-19 20:08:37 +0530

    L-63 done, Like and Unlike Functionality implemented

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import CreateProfile from "./components/profile-forms/CreateProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Posts from "./components/posts/Posts";
 //read both names again
 import Profile from "./components/profile/Profile";
 
@@ -63,6 +64,7 @@ function App() {
             path="/add-education"
             element={<PrivateRoute component={AddEducation} />}
           />
+          <Route path="/posts" element={<PrivateRoute component={Posts} />} />
         </Routes>
       </Router>
     </Provider>
